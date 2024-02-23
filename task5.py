dd020f4ae2cb17ac4aaa3ebe9d55ec19aa494ba6
Denis <shumik16@gmail.com>
2024-02-23 20:26:18 +0300

    first commit

---
+++ task5.py
@@ -0,0 +1,12 @@
+N = [3]
+M1 = int(2)
+languages_1 = {1: "Russian", 2: "English"}
+M2 = int(3)
+languages_2 = {1: "Russian", 2: "Belarusian", 3: "English"}
+print(M1 + M2)
+print(languages_2)
+print(languages_1)
+
+
+
+
